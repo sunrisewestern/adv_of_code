2cf9296f1e974bfe86f107acf865fbef622579e9
sunrisewestern <chenxiaoxime@gmail.com>
2022-12-20 01:45:27 -0500

    day 13

--- day13.py
+++ day13.py
@@ -22,24 +22,37 @@ def check_order(left, right):
             j = right[k]
         except IndexError:
             return False
+        # print(f"i={i},j={j}")
         match i, j:
             case int(), int():
-                if i <= j:
+                if i < j:
                     return True
-                else:
+                elif i > j:
                     return False
+                else:
+                    continue
             case int(), list():
-                check_order([i], j)
+                res = check_order([i], j)
+                if isinstance(res, bool):
+                    return res
             case list(), int():
-                check_order(i, [j])
+                res = check_order(i, [j])
+                if isinstance(res, bool):
+                    return res
             case list(), list():
-                check_order(i, j)
+                res = check_order(i, j)
+                if isinstance(res, bool):
+                    return res
             case _:
                 raise ValueError("wrong input")
 
 
-# f = open("./day13_input.txt", "r").read().split("\n\n")
-f = open("./day13_example.txt", "r").read().split("\n\n")
+# print(check_order([[1], [2, 3, 4]], [[1], 4]))
+# print(check_order([[2, 3, 4]], [[4]]))
+# check_order([9], [8])
+
+f = open("./day13_input.txt", "r").read().split("\n\n")
+# f = open("./day13_example.txt", "r").read().split("\n\n")
 
 pairs_ls = []
 for line in f:
@@ -52,9 +65,18 @@ is_right_order = []
 for idx, pair in enumerate(pairs_ls):
     if check_order(pair[0], pair[1]):
         is_right_order.append(idx + 1)
-    else:
-        print(pair[0])
-        print(pair[1])
-        print()
 
 print(sum(is_right_order))
+
+before_2 = 0
+after_6 = 0
+for idx, pair in enumerate(pairs_ls):
+    for i in pair:
+        if check_order(i, [[2]]):
+            before_2 += 1
+        if check_order([[6]], i):
+            after_6 += 1
+
+print(before_2 + 1)
+print(len(pairs_ls) * 2 - after_6 + 2)
+print((before_2 + 1) * (len(pairs_ls) * 2 - after_6 + 2))
